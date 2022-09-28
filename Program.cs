4ab2c92a0d041359673a880691fa2421ab5565dd
Olga B <>
2022-09-29 00:19:30 +0300

    пример 3 и 4

--- Program.cs
+++ Program.cs
@@ -2,11 +2,23 @@
 //Console.WriteLine("Hello, World!"); - Пример 1
 
 //Пример 2:
-Console.Write("Введите ваше имя"); //Console.Write() - вывод в одну строку
+/*Console.Write("Введите ваше имя"); //Console.Write() - вывод в одну строку
 string username = Console.ReadLine();
 Console.WriteLine("Привет, "); //Console.WriteLine() - в конце перейти на новую строку
-Console.WriteLine(username);
+Console.WriteLine(username);*/
 
 //Console.ReadLine() - считать строку из терминала
 
 //Пример 3: Сложить 2 числа
+int a = 3;
+int b = 5;
+Console.WriteLine(a+b);
+
+//Пример 4: Разделить 2 числа 
+int c = 12;
+int d = 5;
+Console.WriteLine(c/d);
+
+double i = 12;
+double f = 5;
+Console.WriteLine(i/f);
