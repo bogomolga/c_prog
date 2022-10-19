3032debd0852d1e4a65329e787836694d1f889b9
Olga B <>
2022-10-20 00:11:50 +0300

    new

---
+++ lesson_4/home_work_2/Program.cs
@@ -0,0 +1,43 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+int a = PrintAndGetValue("Задача 27. Введите положительное число: ");
+
+bool isValid = isPositiveNumber(a);
+if(isValid)
+{
+    //double s = CalculateStepen(a,b);
+    Console.WriteLine("Cуммa цифр в числе = "+CalculateDigitsSum(a));
+}
+else
+{
+    Console.WriteLine("Неправильные данные");
+}
+
+//---------------------------------------------------------------------------------------
+int PrintAndGetValue(string message) //функция для ввода и считывания значения из консоли
+{
+    Console.WriteLine(message);
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+bool isPositiveNumber(int number)
+{
+    if(number <=1)
+        return false;
+    return true;
+}
+
+int CalculateDigitsSum(int number) //Cуммa цифр в числе
+{
+    int s=0;
+    while (number>0) 
+    {
+        s=s+(number % 10);
+        number /=10;
+    }
+    return s;
+}
