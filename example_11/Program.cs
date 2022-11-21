570884c1329160f841a7fb083b0f527c1bfea8e7
Olga B <>
2022-11-22 00:40:06 +0300

    new

--- example_11/Program.cs
+++ example_11/Program.cs
@@ -1,4 +1,15 @@
 ﻿//Пример 11. Метод = Функция у них. Заполнить массив автоматически и распечатать его.
+
+int[] array = new int[10]; //создай новый массив в котором будет 10 элементов. Будет заполнен нулями
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4); //ищем число 4 в сгенеренном массиве и выводим в терминал его индекс
+Console.WriteLine(pos); //выведет 0 если такого числа в массиве не будет найдено! Чтоб этого избежать -> int position = -1; Тогда выведет -1 в консоль - "элемент не найден"
+
+//------------------------
 void FillArray(int[] collection) // void - метод ничего не возвращает это viod метод
 {
     int length = collection.Length;
@@ -10,14 +21,22 @@ void FillArray(int[] collection) // void - метод ничего не возв
     }
 }
 
-void PrintArray(int[] col) //вывод массива в консоль
+void PrintArray(int[] collection)
 {
-    int count = col.Length;
-    int position = 0;
-    while (position < count)
+    int length=collection.Length;
+    int index=0;
+    Console.Write("Массив: [");
+    while(index<length)
     {
-        Console.WriteLine(col[position]);
-        position++;
+        if (index!=length-1)
+        {
+            Console.Write(collection[index]+", ");
+        }
+        else
+        {
+            Console.Write(collection[index]+"]");
+        }
+        index++;
     }
 }
 
@@ -38,13 +57,16 @@ int IndexOf(int[] collection, int find) //Поиск 1го вхождения з
     return position;
 }
 
-int[] array = new int[10]; //создай новый массив в котором будет 10 элементов. Будет заполнен нулями
-
-FillArray(array);
-PrintArray(array);
-Console.WriteLine();
-
-int pos = IndexOf(array, 4); //ищем число 4 в сгенеренном массиве и выводим в терминал его индекс
-Console.WriteLine(pos); //выведет 0 если такого числа в массиве не будет найдено! Чтоб этого избежать -> int position = -1; Тогда выведет -1 в консоль - "элемент не найден"
 
 
+//Пример: Задача из собеседования - Сколько раз выполнится цикл?
+
+int n = 10;
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+    int j = i;
+    i=i+j;
+    count++;
+    Console.WriteLine("i= " + i);
+}   Console.WriteLine("Цикл выполнился " + count + " раз");
